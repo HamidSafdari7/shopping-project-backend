8646d92c5ae41648f6db89265f653ce3b84ab4fc
HamidSafdari7 <hamidsafdari159@gmail.com>
2022-08-18 19:35:34 +0430

    Update cart.js

--- cart.js
+++ cart.js
@@ -24,4 +24,16 @@ router.post('/', (req, res) => {
 
   });
 
+  //delete
+
+router.delete('/:id', (req,res)=>{
+
+    const order = orders.find(c => c.id ===parseInt(req.params.id));
+    if(!order) return res.status(404).send('404');
+
+    const index = orders.indexOf(order);
+    orders.splice(index , 1);
+    res.send({error:false , order});
+});
+
   module.exports=router;
